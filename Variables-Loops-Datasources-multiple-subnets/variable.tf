9fc67eee9e275e86d5a395ad49dc2f691c6ceaf4
ramudu <venkataramudu.gaddam@totmtech.in>
2023-12-10 16:57:32 +0530

    list, count, index variables addes

--- Variables-Loops-Datasources-multiple-subnets/variable.tf
+++ Variables-Loops-Datasources-multiple-subnets/variable.tf
@@ -7,12 +7,12 @@ variable "vpc_cidr" {
 }
 
 variable "subnet_cidr" {
-  type = "list"
+  type = list(string)
   default = ["190.160.1.0/24","190.160.2.0/24","190.160.3.0/24"]
 }
 
 variable "azs" {
-  type = "list"
+ type = list(string)
   default = ["us-west-2a","us-west-2b","us-west-2c"]
 }
 
